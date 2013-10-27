a9b74045b1cd29528127b2c806d33a0f7a7e0096
Yung-Ju Chang <mr.armuro@gmail.com>
2013-10-27 15:55:09 -0400

    add askview controller

---
+++ kiptalk/AskViewController.h
@@ -0,0 +1,18 @@
+//
+//  AskViewController.h
+//  kiptalk
+//
+//  Created by Armuro on 10/26/13.
+//  Copyright (c) 2013 Armuro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AskViewController : UIViewController
+
+
+
+
+
+
+@end
---
+++ kiptalk/AskViewController.m
@@ -0,0 +1,64 @@
+//
+//  AskViewController.m
+//  kiptalk
+//
+//  Created by Armuro on 10/26/13.
+//  Copyright (c) 2013 Armuro. All rights reserved.
+//
+
+#import "AskViewController.h"
+
+@interface AskViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextView *questionContentTextView;
+
+
+@end
+
+@implementation AskViewController
+
+
+- (void) updateUI{
+    
+    NSLog(@"update the UI");
+}
+
+
+//this function enlables camera and add the new taken photo to the questionContextTextView
+- (IBAction)takePhoto{
+    
+    
+    [self updateUI];
+    
+}
+
+
+- (IBAction)recordAudio {
+
+    [self updateUI];
+
+}
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
